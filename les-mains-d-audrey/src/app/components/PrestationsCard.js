a58636d62582b7fe8bc3a45ed3d6c762645233c7
luciel53 <l.leroty@gmail.com>
2024-11-16 16:06:10 +0100

    fixed the cards and grid

--- les-mains-d-audrey/src/app/components/PrestationsCard.js
+++ les-mains-d-audrey/src/app/components/PrestationsCard.js
@@ -1,13 +1,18 @@
 import Button from "./Button";
 
-export default function PrestationsCard({ title, text }) {
+export default function PrestationsCard({ title, p1, p2, p3, p4, p5, p6 }) {
   return (
-    <div className="flex flex-col bg-lightBG border border-lightPink rounded-3xl h-96 lg:w-96 justify-between md:justify-between mt-1 drop-shadow-sm transition-transform duration-200 ease-in-out hover:scale-105 hover:drop-shadow-lg">
+    <div className="flex flex-col bg-lightBG border border-lightPink rounded-3xl h-96 lg:w-80 xl:w-96 justify-between md:justify-between mt-1 drop-shadow-sm transition-transform duration-200 ease-in-out hover:scale-105 hover:drop-shadow-lg">
       <h2 className="text-center font-italiana text-pink w-60 mx-auto text-2xl md:text-3xl mt-2">
         {title}
       </h2>
 
-      <p className="w-[90%] mx-auto">{text}</p>
+      <p className="w-[90%] mx-auto">{p1}</p>
+      <p className="w-[90%] mx-auto">{p2}</p>
+      <p className="w-[90%] mx-auto">{p3}</p>
+      <p className="w-[90%] mx-auto">{p4}</p>
+      <p className="w-[90%] mx-auto">{p5}</p>
+      <p className="w-[90%] mx-auto">{p6}</p>
 
       <div className="mx-auto mb-4">
         <Button btnText="Je réserve" />
--- les-mains-d-audrey/src/app/services/page.js
+++ les-mains-d-audrey/src/app/services/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import PrestationsCard from "../components/PrestationsCard";
+import Button from "../components/Button";
 
 export const metadata = {
   title: "Les mains d'Audrey - Ateliers Bébé Signe",
@@ -31,74 +32,79 @@ export default function Prestations() {
       </section>
       {/* Grid */}
       <section>
-        <div className="grid grid-cols md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 w-[86%] md:w-[89%] lg:w-[81%] mx-auto md:mt-2">
+        <div className="grid grid-cols md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 w-[86%] md:w-[89%] lg:w-[95%] xl:w-[81%] mx-auto md:mt-2">
           <PrestationsCard
             title="Ateliers Bébé signe
 individuel"
-            text="En individuel - 1 famille (2 parents possibles et bébé)
-Un cycle de 4 ateliers ou l un des 4 ateliers au
-choix : (journée, activités, environnement et
-émotions de bébé)
-45 minutes
-45€ L’UNITÉ - 150€ LE CYCLE"
+            p1="En individuel - 1 famille (2 parents possibles et bébé)"
+            p2="Un cycle de 4 ateliers ou l un des 4 ateliers au
+            choix : (journée, activités, environnement et
+            émotions de bébé)"
+            p3="45 minutes"
+            p4="45€ L'UNITÉ - 150€ LE CYCLE"
           />
           <PrestationsCard
             title="Ateliers Bébé signe
           en collectif"
-            text="En individuel - 1 famille (2 parents possibles et bébé)
-Un cycle de 4 ateliers ou l un des 4 ateliers au
-choix : (journée, activités, environnement et
-émotions de bébé)
-45 minutes
-45€ L’UNITÉ - 150€ LE CYCLE"
+            p1="En collectif - 3 familles maximum"
+            p2="Un cycle de 4 ateliers ou l un des 4 ateliers au
+              choix : (journée, activités, environnement et
+              émotions de bébé)"
+            p3="45 minutes"
+            p4="30€ L'UNITÉ - 100€ LE CYCLE"
           />
-          <PrestationsCard
-            title="L’heure de conte signé"
-            text="En individuel - 1 famille (2 parents possibles et bébé)
-Un cycle de 4 ateliers ou l un des 4 ateliers au
-choix : (journée, activités, environnement et
-émotions de bébé)
-45 minutes
-45€ L’UNITÉ - 150€ LE CYCLE"
-          />
+          <div className="flex flex-col bg-lightBG border border-lightPink rounded-3xl h-96 lg:w-80 xl:w-96 justify-between md:justify-between mt-1 drop-shadow-sm transition-transform duration-200 ease-in-out hover:scale-105 hover:drop-shadow-lg">
+            <h2 className="text-center font-italiana text-pink w-60 mx-auto text-2xl md:text-3xl mt-2">
+              L’heure de conte signé
+            </h2>
+
+            <p className="w-[90%] mx-auto">À votre domicile</p>
+            <p className="w-[90%] mx-auto">
+              Ateliers sur mesure dans les collectivités (bibliothèques, écoles,
+              crèches, etc.)
+            </p>
+            <p className="w-[90%] mx-auto">
+              Pour toute autre demande me contacter
+            </p>
+            <p className="w-[90%] mx-auto">SUR DEVIS</p>
+
+            <div className="mx-auto mb-4">
+              <Button btnText="Contactez-moi" />
+            </div>
+          </div>
           <PrestationsCard
             title="Ateliers comptines signées"
-            text="En individuel - 1 famille (2 parents possibles et bébé)
-Un cycle de 4 ateliers ou l un des 4 ateliers au
-choix : (journée, activités, environnement et
-émotions de bébé)
-45 minutes
-45€ L’UNITÉ - 150€ LE CYCLE"
+            p1="De la naissance, voire même dès la grossesse (certains parents préfèrent apprendre les comptines avant l'arrivée de bébé), jusqu'à ce que l'enfant n'aime plus faire des comptines (6-8 ans ou plus)"
+            p2="en indivuel : 35€"
+            p3="en collectif (max 3 familles): 30€"
           />
           <PrestationsCard
             title="Ateliers à thème
           (sur mesure selon les saisons)"
-            text="En individuel - 1 famille (2 parents possibles et bébé)
-Un cycle de 4 ateliers ou l un des 4 ateliers au
-choix : (journée, activités, environnement et
-émotions de bébé)
-45 minutes
-45€ L’UNITÉ - 150€ LE CYCLE"
+            p1="⇾ en individuel : 45€ (parents et deux enfants) adulte (hors parents du bébé): 20€
+            enfant supplémentaire : 5€"
+            p2="⇾ en collectif (min 2 familles - max 3) : 30€ (parents et deux enfants)
+            adulte hors famille: 15€"
           />
           <PrestationsCard
             title="Carte cadeau"
-            text="En individuel - 1 famille (2 parents possibles et bébé)
-Un cycle de 4 ateliers ou l un des 4 ateliers au
-choix : (journée, activités, environnement et
-émotions de bébé)
-45 minutes
-45€ L’UNITÉ - 150€ LE CYCLE"
+            p1="Vous voulez offrir un atelier à une personne de votre entourage ? Un atelier ou un cycle de 4 ateliers. Valable pour un an"
           />
-          <PrestationsCard
-            title="Ateliers Bébé signe
-individuel"
-            text="En individuel - 1 famille (2 parents possibles et bébé)
-Un cycle de 4 ateliers ou l un des 4 ateliers au
-choix : (journée, activités, environnement et
-émotions de bébé)
-45 minutes
-45€ L’UNITÉ - 150€ LE CYCLE"
-          />
+          <div className="flex flex-col bg-lightBG border border-lightPink rounded-3xl h-96 lg:w-80 xl:w-96 justify-between md:justify-between mt-1 drop-shadow-sm transition-transform duration-200 ease-in-out hover:scale-105 hover:drop-shadow-lg">
+            <h2 className="text-center font-italiana text-pink w-60 mx-auto text-2xl md:text-3xl mt-2">
+              L’heure de conte signé
+            </h2>
+
+            <p className="w-[90%] mx-auto">
+              Je m'adapte en vous proposant des activités sur-mesure. Pour tout
+              renseignement, contactez-moi
+            </p>
+            <p className="w-[90%] mx-auto">SUR DEVIS</p>
+
+            <div className="mx-auto mb-4">
+              <Button btnText="Contactez-moi" />
+            </div>
+          </div>
         </div>
       </section>
     </div>
