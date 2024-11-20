daee972eabaa87f2c6ee5dbb455328bdd2b82cf6
luciel53 <l.leroty@gmail.com>
2024-11-20 17:27:03 +0100

    finished the front end of events

--- les-mains-d-audrey/src/app/evenements/page.js
+++ les-mains-d-audrey/src/app/evenements/page.js
@@ -34,42 +34,54 @@ export default function Event() {
         </h3>
       </section>
       <section>
-        <div className="flex flex-row bg-lightBG rounded-xl w-80 h-32 z-10 relative mx-auto border border-lightPink drop-shadow-lg">
-          <div className="flex flex-row">
-            image
-            <div className=" flex flex-col justify-center items-center bg-lightPink h-12 w-12">
-              <span>date</span>
+        <div className="flex flex-row bg-lightBG rounded-xl w-[95%] lg:w-[50%] h-32  mx-auto border border-lightPink drop-shadow-lg mb-4">
+          <div className="relative z-0 w-[40%] h-full overflow-hidden">
+            <Image
+              src="/images/test.jpg"
+              alt="Atelier évènement organisé par les mains d'Audrey - Bébé signe"
+              fill
+              className="object-cover rounded-l-xl"
+            />
+            <div className="flex flex-col absolute z-10 right-0 items-center bg-lightPink h-12 w-12 rounded-es-xl">
+              <span className="font-italiana w-8 text-center font-bold">
+                7 Nov
+              </span>
             </div>
           </div>
-          <div className="px-3">
-            <h4 className=" font-italiana text-pink text-xl mb-1">
-              Nom de l evenement
+
+          <div className="px-3 flex flex-col justify-around">
+            <h4 className="font-italiana text-pink text-xl mb-2">
+              Nom de l'évènement
             </h4>
-            <div className="flex flex-row ">
+            <div className="flex flex-row">
               <Image
                 src="/images/icons/location.png"
                 alt="Lieu de l'évènement organisé par les mains d'Audrey"
                 width={50}
                 height={50}
-                className="h-5 md:h-8 w-5 md:w-8"
+                className="h-5 md:h-8 lg:h-6 w-5 md:w-8 lg:w-6 mr-2"
               />
-              <p>lieu</p>
+              <p className="font-jaldi">
+                75 rue du général de Gaulle, 86000 Nullepart
+              </p>
             </div>
-            <div className="flex flex-row ">
-              <Image
-                src="/images/icons/clock.png"
-                alt="Lieu de l'évènement organisé par les mains d'Audrey"
-                width={50}
-                height={50}
-                className="h-5 md:h-8 w-5 md:w-8"
-              />
-              <p>heure</p>
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row">
+                <Image
+                  src="/images/icons/clock.png"
+                  alt="Heure de l'évènement organisé par les mains d'Audrey"
+                  width={50}
+                  height={50}
+                  className="h-5 md:h-8 lg:h-6 w-5 md:w-8 lg:w-6 mr-2"
+                />
+                <p className="font-jaldi">15h00</p>
+              </div>
+              <Link href="/contact">
+                <button className="bg-lightBG mt-3 mb-2 border border-gold rounded-full font-italiana text-gold px-2 hover:text-lightBG hover:bg-gold">
+                  Je réserve
+                </button>
+              </Link>
             </div>
-            <Link href="/contact">
-              <button className="bg-lightBG border border-gold rounded-full font-italiana text-gold px-2 hover:text-lightBG hover:bg-gold">
-                Je réserve
-              </button>
-            </Link>
           </div>
         </div>
       </section>
