0012bdb30416c978d522d332072ad3bbb3ad720e
luciel53 <l.leroty@gmail.com>
2024-11-22 19:26:57 +0100

    created a netlify directory

--- les-mains-d-audrey/next.config.mjs
+++ les-mains-d-audrey/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,  // Désactive l'optimisation des images
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
